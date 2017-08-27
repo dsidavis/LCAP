ca7447db69fd3fd0d769c16df14e88b69bf07d09
Duncan Temple Lang <duncan@r-project.org>
2017-08-26 18:03:58 -0700

    find the grey rectangle and hence the region for the % number

--- R2/section3.R
+++ R2/section3.R
@@ -1,6 +1,9 @@
 getSupplementalTotal =
 function(file, doc = readPDFXML(file))
 {
+    if(isScanned2(doc))
+       return(NA)
+    
     # Use of Supplemental and Concentration Grant funds and Proportionality
 #   sec3 = getNodeSet(doc, "//text[starts-with(., 'Section 3: Use of')]")
 
@@ -35,10 +38,51 @@ getPctIncrease =
 function(file, doc = readPDFXML(file))
 {
   ll = getNodeSet(doc, "//text[ contains(normalize-space(.), 'Consistent with the requirements of 5 CCR 15496') ]")
+  if(length(ll) == 0)
+      ll = getNodeSet(doc, "//text[contains(., 'Consistent')]")
   p = xmlParent(ll[[1]])
-  getNodeSet(p, ".//text[isPct(string(.))]", xpathFuns = list(isPct = isPctString))
+
+  nodes = getNodeSet(p, ".//text[isPct(string(.))] | .//text[isNumber(string(.)) and starts-with(following-sibling::text[1], '%')]", xpathFuns = list(isPct = isPctString, isNumber = isNumber))
+
+  if(length(nodes) > 1) {
+      bb = getBBox2(nodes)
+      box = getPctBox(p)
+#  browser()      
+      ok = bb[,1] > box[1] & bb[,1] + bb[,3] < box[3] & bb[, 2] > box[2] & bb[,2] + bb[,4] < box[4]
+      nodes = nodes[ok]
+  }
+  
+  nodes
 }
 
 isPctString =
 function(str)
-    grepl("^[0-9]+\\.[0-9]{2} *%? *$", str)
+  grepl("^[[:space:]]*[0-9]+\\.[0-9]{1,2} *%? *$", str)
+
+isNumber =
+    #XXX This will generate warnings when it returns false.
+function(x)
+  !is.na(as.numeric(x))
+
+
+getPctBox =
+function(page)
+{
+    nodes = getNodeSet(page, ".//rect | .//line")
+    bx = getBBox(nodes)
+    col = sapply(nodes, xmlGetAttr, "fill.color")
+    colored = (col != "0,0,0")
+    tmp = bx[colored, ]
+    cb = c(range(tmp[, c(1,3)]), range(tmp[, c(1,3)+1]))
+    wd = bx[,3] - bx[,1]
+    ht = bx[,4] - bx[,2]
+
+    hi = bx[,2] >= cb[3] & bx[,2] <= cb[4]
+    wi = bx[,3] <= cb[1]
+    z = bx[hi & wi, , drop = FALSE]
+
+    return(c(x0 = min(z[,1]), y0 = min(z[,2]), x1 = max(z[,3]), y1= max(z[, 4])))
+#    return( matrix(c(min(z[,1]), min(z[,2]), max(z[,3]), max(z[, 4])), 1, 4) )
+    i = wd > 20 & wd < 500 & ht > 10 & ht < 40
+    bx[i,, drop = FALSE]
+}
